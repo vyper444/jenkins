75938fdef98daa43c8ba72247fc88fc63389346e
Raghavendra P <444raghav@gmail.com>
2020-05-09 21:49:01 +0530

    new changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express
+const app = express()
 const port = 3080
 
 app.get('/', (req, res) => res.send('Hello world'))
 
-app.listen(port, () => console.log("Connected by port ${port}"))
+app.listen(port, () => console.log('Connected by port ${port}'))
