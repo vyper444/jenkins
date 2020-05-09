4a87a3bfab6772facc3d5edbc797cb7968fe95d2
Raghavendra P <444raghav@gmail.com>
2020-05-09 21:38:05 +0530

    port change

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express
-const port = 3000
+const port = 3080
 
 app.get('/', (req, res) => res.send('Hello world'))
+
 app.listen(port, () => console.log("Connected by port ${port}"))
